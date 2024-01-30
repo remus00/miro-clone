b6b9e40668a8db44f86f892847fe5c88e1469871
remus00 <135813551+remus00@users.noreply.github.com>
2024-01-30 22:47:21 +0100

    Sidebar & Navbar

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const OrganizationSidebar = () => {
+    return (
+        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+            OrganizationSidebar
+        </div>
+    );
+};
---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+    return (
+        <aside className="text-white fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 ">
+            side
+        </aside>
+    );
+};
